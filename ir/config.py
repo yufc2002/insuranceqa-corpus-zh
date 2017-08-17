d723ddd55bdbbc8e5f44984c33d218d5221c3e1e
FrankBlood <1062947208@qq.com>
2017-08-17 22:49:42 +0800

    my code based keras.

---
+++ ir/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import division
+
+__copyright__ = "Copyright (c) 2017 Guoxiu He. All Rights Reserved"
+__author__ = "Guoxiu He"
+__date__ = "2017-08-08:18:32:05"
+
+class Config(object):
+    def __init__(self):
+        print("config...")
